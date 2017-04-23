ae6900f46623220974ef807d4743cfc68997813c
Stephen Scott <me@suchipi.com>
2017-04-22 19:41:41 -0600

    Update repository url, scaffold example

--- scaffold/morpher-transforms.js
+++ scaffold/morpher-transforms.js
@@ -1,25 +1,29 @@
 // You can define transforms for the `morpher` package here. You can run them
 // via the "Morpher: Open Transforms List" command in the command palette.
-// See http://github.com/suchipi/morpher for more info about writing transforms.
+// See https://github.com/suchipi/atom-morpher/blob/master/README.md for more
+// info about writing transforms.
 
 module.exports = function() {
   return [
-    /* Example transform: uppercases text in the current selection
+    /* Example transform: uppercases selected text
     {
-      id: "uppercase-selection",
       name: "Uppercase selected text",
       description: "Converts all characters in the selection to uppercase",
-      onSelected: function({ text, selection }) {
+      onSelected({ text, selection }) {
+        // This transformer will use atom's TextBuffer to transform the text,
+        // but you can use any method you want; you just need to make a
+        // string containing all the new text you want to be in the buffer.
         const { TextBuffer } = require("atom");
-
         const buffer = new TextBuffer({ text });
         const selectionContent = buffer.getTextInRange(selection);
         buffer.setTextInRange(selection, selectionContent.toUpperCase());
+        const newText = buffer.getText();
+
         return {
-          text: buffer.getText(),
+          text: newText,
         };
-      }
-    }
+      },
+    },
     */
   ];
 };
